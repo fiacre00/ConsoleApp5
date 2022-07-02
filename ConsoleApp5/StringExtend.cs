42df50be95ec4d2277dbd8808782e386a43bed42
Fiacre <info@fiacre.net>
2022-07-02 16:13:59 +0100

    test change

--- ConsoleApp5/StringExtend.cs
+++ ConsoleApp5/StringExtend.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp5
         public static List<int> AllIndexesOf(this string str, string value)
         {
             if (String.IsNullOrEmpty(value))
-                throw new ArgumentException("the string to find may not be empty", "value");
+                throw new ArgumentException("The string to find may not be empty", "value");
             List<int> indexes = new List<int>();
             for (int index = 0; ; index += value.Length)
             {
